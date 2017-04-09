a43bba4bf68ef3b45d94064dd2ad90be8c4ace6e
Dawid Urbanski <git@dawidurbanski.pl>
2017-04-09 18:13:13 +0200

    StatusReportEx dock

--- usb2lin06Controler.h
+++ usb2lin06Controler.h
@@ -31,6 +31,10 @@ namespace usb2lin06
 
 struct StatusReportEx : public StatusReport//NOTE: this is a workaround struct from usb2lin06.h cannot have functions due to usage in kernel
 {
+/*
+ * height is a 16 signed integer with the height in 1/10 mm with 0 as lowest height of actuators
+ * offsetCM - bottom position of you desk
+ */
   float getHeightCM(float offsetCM=0.0f) const;
 
 /*
